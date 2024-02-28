cccf6aed9e0161f847c3188a7f1a76539cf35213
sakshi.shah <sakshi.shah@drcsystems.com>
2024-02-28 12:47:17 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// const heading=React.createElement("h1",
+// {id:"heading"},
+// "hello sakshi shah")
+// console.log(heading) //object
+//     const root= ReactDOM.createRoot(document.getElementById("root"))
+//     root.render(heading)
+/**
+ * <div id="p"> 
+  *<div id="c">
+    *<p>Different </p> 
+     *<p>Different </p>
+    *</div>
+     *<div id="c2">
+    *<p>Different </p>
+     *<p>Different </p>
+    *</div>
+*</div>
+ * 
+ * 
+ * 
+ 
+ */
+const heading = React.createElement("div",{id:"p"},
+                        [ React.createElement("div",{id:"c"},
+                                 [React.createElement("h1",{},"hello multiple"),
+                                 React.createElement("h2",{},"hello 2nd multiple")]
+                                 ),
+                        React.createElement("div",{id:"c2"},
+                                 [React.createElement("h1",{},"hello 3multiple"),
+                                 React.createElement("h2",{},"hello 4nd multiple")]
+                                 )
+                                ]
+                                 )
+console.log(heading) //object
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
